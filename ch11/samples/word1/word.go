d6f732e41aaac3b82d042177a5060990016bfcd1
shino <harimanmon@gmail.com>
2017-11-17 00:59:57 +0900

    feat: ch11 samples

---
+++ ch11/samples/word1/word.go
@@ -0,0 +1,15 @@
+// パッケージwordは、言葉遊び用のユーティリティを提供します。
+package word
+
+// ISPalindromeはsが前からでも後ろからでも同じように読めるかどうかを報告します。
+// (最初の試みのバージョン)
+func IsPalindrome(s string) bool {
+	for i := range s {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+
