7f6ee2d810227b2ddeda004c175db2481461de16
shino <harimanmon@gmail.com>
2017-12-23 22:17:11 +0900

    solve: ch05/ex05

---
+++ ch05/ex05/run.bash
@@ -0,0 +1,3 @@
+#!/bin/sh
+go build countWordsAndImages.go
+./countWordsAndImages https://golang.org https://yahoo.co.jp
--- ch05/samples/findlinks2/findlinks2.go
+++ ch05/samples/findlinks2/findlinks2.go
@@ -5,17 +5,38 @@ import (
 	"os"
 
 	"golang.org/x/net/html"
+	"net/http"
 )
 
 func main() {
-	doc, err := html.Parse(os.Stdin)
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
-		os.Exit(1)
+		return nil, err
 	}
-	for _, link := range visit(nil, doc) {
-		fmt.Println(link)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
 	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
+
 }
 
 // visit appends to links each link found in n and returns the result.
